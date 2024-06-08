e7940267574b7367d833087cf4fc9934c502f615
GannuNb <155215289+GannuNb@users.noreply.github.com>
2024-06-08 13:51:02 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: 'https://authentication-frontend-mu.vercel.app', // Adjust as necessary
+    origin: 'https://authentication-frontend-mu.vercel.app/', // Adjust as necessary
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
